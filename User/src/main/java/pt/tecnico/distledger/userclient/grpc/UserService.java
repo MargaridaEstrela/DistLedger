911e4d89b06b05caf0abd89c3951f4727aa8c5cb
MargaridaEstrela <margarida.e2002@gmail.com>
2023-03-08 17:14:38 +0000

    feat: handle ResponseCode and extend the debug funtion

--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -1,7 +1,6 @@
 package pt.tecnico.distledger.userclient.grpc;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import pt.tecnico.distledger.userclient.UserClientMain;
 import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc;
 import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.BalanceRequest;
 import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.BalanceResponse;
@@ -27,81 +26,115 @@ public class UserService {
      */
 
     private UserServiceGrpc.UserServiceBlockingStub stub;
+    private ResponseCode code;
 
     public UserService(UserServiceGrpc.UserServiceBlockingStub stub) {
         this.stub = stub;
+        code = ResponseCode.UNRECOGNIZED;
     }
 
-    public void createAccount(String server, String username) {
+    public ResponseCode get_code() { return code; }
 
-        ResponseCode res_code;
+    public void set_code(ResponseCode code) { this.code = code; }
+    
+    public ResponseCode createAccount(String server, String username) {
 
         try {
             CreateAccountRequest createAccRequest = CreateAccountRequest.newBuilder().setUserId(username).build();
             CreateAccountResponse createAccResponse = this.stub.createAccount(createAccRequest);
 
-            // get response code
+            ResponseCode code = createAccResponse.getCode();
 
+            if (code == ResponseCode.OK) {
+               UserClientMain.debug("User " + username + " created an account"); 
+            } else if (code == ResponseCode.USER_ALREADY_EXISTS) {
+                UserClientMain.debug("User " + username + " already exists"); 
+            }
+            return code;
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
                     e.getStatus().getDescription());
         }
 
+        return ResponseCode.UNRECOGNIZED;
+
     }
 
-    public void deleteAccount(String server, String username) {
-
-        ResponseCode res_code;
+    public ResponseCode deleteAccount(String server, String username) {
 
         try {
             DeleteAccountRequest deleteAccRequest = DeleteAccountRequest.newBuilder().setUserId(username).build();
             DeleteAccountResponse deleteAccResponse = this.stub.deleteAccount(deleteAccRequest);
 
-            // get response code
+             ResponseCode code = deleteAccResponse.getCode();
 
+            if (code == ResponseCode.OK) {
+               UserClientMain.debug("User " + username + " deleted the account"); 
+            } else if (code == ResponseCode.NON_EXISTING_USER) {
+                UserClientMain.debug("User " + username + " doesn't exist"); 
+            }
+            return code;
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
                     e.getStatus().getDescription());
         }
+
+        return ResponseCode.UNRECOGNIZED;
     }
 
-    public void balance(String server, String username) {
+    public List<Integer> balance(String server, String username) {
 
         int balance = -1;
-        ResponseCode res_code = ResponseCode.UNRECOGNIZED;
+        List<Integer> res = new ArrayList<Integer>();
 
         try {
             BalanceRequest balanceRequest = BalanceRequest.newBuilder().setUserId(username).build();
             BalanceResponse balanceResponse = this.stub.balance(balanceRequest);
 
+            ResponseCode code = balanceResponse.getCode();
             balance = balanceResponse.getValue();
 
-            // get response code
+            res.add(code.getNumber());
+            res.add(balance);
 
+            if (code == ResponseCode.NON_EXISTING_USER) {
+                 UserClientMain.debug("User " + username + "doesn't exist"); 
+            }
+
+             return res;
 
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
             e.getStatus().getDescription()); 
         }
 
-        if (res_code == ResponseCode.OK) {
-            System.out.println(balance);
-        }
+        res.add(ResponseCode.UNRECOGNIZED.getNumber());
+        res.add(balance);
+
+        return res;
     }
 
-    public void transferTo(String server, String from, String dest, Integer amount) {
+    public ResponseCode transferTo(String server, String from, String dest, Integer amount) {
         
-        ResponseCode res_code;
-
         try {
             TransferToRequest transferToRequest = TransferToRequest.newBuilder().setAccountFrom(from).setAccountTo(dest).setAmount(0).build();
             TransferToResponse transferToResponse = this.stub.transferTo(transferToRequest);
 
-            // get response code
+            ResponseCode code = transferToResponse.getCode();
 
+            if (code == ResponseCode.OK) {
+               UserClientMain.debug("User " + from + " transfered " + amount + " to user " + dest); 
+            } else if (code == ResponseCode.NON_EXISTING_USER) {
+                UserClientMain.debug("Something wrong with users"); 
+            } else if (code == ResponseCode.AMOUNT_NOT_SUPORTED) {
+                UserClientMain.debug("User " + from + " doesn't have enought money"); 
+            }
+            return code;
         } catch (StatusRuntimeException e) {
             System.out.println("Caught exception with description: " +
             e.getStatus().getDescription()); 
         }
+
+        return ResponseCode.UNRECOGNIZED;
     }
 }
