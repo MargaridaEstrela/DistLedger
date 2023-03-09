8b82db4063dee572e57dbac2bd429ca3e6388a36
Rui Fonseca <rui.f.fonseca@tecnico.ulisboa.pt>
2023-03-09 08:05:59 +0000

    add getLedgerState to AdminServiceImpl

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/AdminServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/AdminServiceImpl.java
@@ -3,7 +3,10 @@ package pt.tecnico.distledger.server.domain;
 import io.grpc.stub.StreamObserver;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc.AdminServiceImplBase;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.*;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions;
 import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.*;
+import pt.tecnico.distledger.server.domain.operation.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,9 +58,30 @@ public class AdminServiceImpl extends AdminServiceImplBase {
     @Override
     public void getLedgerState(getLedgerStateRequest request, StreamObserver<getLedgerStateResponse> responseObserver) {
 
+        ArrayList<pt.tecnico.distledger.server.domain.operation.Operation> operations = new ArrayList(server.getLedger());
+        LedgerState.Builder ledger = LedgerState.newBuilder();
+        for (pt.tecnico.distledger.server.domain.operation.Operation operation : operations) {
+            pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Operation.Builder operationContract = DistLedgerCommonDefinitions.Operation.newBuilder();
+            if(operation.getType() == "CREATE") {
+                operationContract.setType(OperationType.OP_CREATE_ACCOUNT);
+            }
+            else if(operation.getType() == "DELETE") {
+                operationContract.setType(OperationType.OP_DELETE_ACCOUNT);
+            }
+            else if(operation.getType() == "TRANSFER") {
+                operationContract.setType(OperationType.OP_TRANSFER_TO);
+                TransferOp transferoperation = (TransferOp) operation;
+                operationContract.setDestUserId(transferoperation.getDestAccount());
+                operationContract.setAmount(transferoperation.getAmount());
+            }
+            else {
+                operationContract.setType(OperationType.OP_UNSPECIFIED);
+            }
+            operationContract.setUserId(operation.getAccount());
+            ledger.addLedger(operationContract);
+        }
         
-
-        getLedgerStateResponse response = getLedgerStateResponse.newBuilder().build();
+        getLedgerStateResponse response = getLedgerStateResponse.newBuilder().setLedgerState(ledger).build();
 
         responseObserver.onNext(response);
             
