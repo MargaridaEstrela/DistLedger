1c0d8f051960c1e5204a0c819e651f2c913dc124
MargaridaEstrela <margarida.e2002@gmail.com>
2023-03-21 11:36:39 +0000

    fix: receiving requestes on impl

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServerServiceImpl.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServerServiceImpl.java
@@ -3,6 +3,7 @@ package pt.tecnico.distledger.namingserver;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.grpc.stub.StreamObserver;
 import pt.tecnico.distledger.namingserver.NamingServer;
 import pt.tecnico.distledger.namingserver.ServerEntry;
 import pt.tecnico.distledger.namingserver.ServiceEntry;
@@ -30,25 +31,32 @@ public class NamingServerServiceImpl {
         }
     } 
 
-    public void register(String serviceName, String type, String port) {
+    public void register(RegisterRequest request, StreamObserver<RegisterResponse> responseObserver) {
         
         if (this.debugFlag) {
-            debug("Register service: " + serviceName + ", " + type + ", " + port + "started");
+            debug("Register service: started");
         }
 
         // Register a request to register
-        RegisterRequest
-        RegisterResponse.Builder response = RegisterResponse.newBuilder();
+        RegisterResponse response;
+        String serviceName = request.getService();
+        String type = request.getType();
+        String address = request.getAddress();
 
         // Check if service name is already registered
         if (!namingServer.getServicesMap().containsKey(serviceName)) {
-            ServerEntry serverEntry = new ServerEntry(serviceName, type, port);
+            ServerEntry serverEntry = new ServerEntry(serviceName, type, address);
             ServiceEntry serviceEntry = new ServiceEntry(serviceName);
             serviceEntry.addServerEntry(serverEntry);
             namingServer.addServerName(serviceName, serviceEntry);
         } 
-        else {
-            System.err.println("ERROR: Not possible to register the server " + serviceName);
+
+        response = RegisterResponse.getDefaultInstance();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+
+        if (this.debugFlag) {
+            debug("Register service: ended");
         }
     }
 
@@ -59,10 +67,9 @@ public class NamingServerServiceImpl {
 
         String serviceName = request.getServiceName();
         String type = request.getType();
-        
-        LookupResponse.Builder response = LookupResponse.newBuilder();
 
-        List<String> result = new ArrayList<String>();
+        LookupResponse.Builder response = LookupResponse.newBuilder();
+        String server;
 
         // Given a service name and a type, return all the servers
         if (namingServer.getServicesMap().containsKey(serviceName)) {
@@ -70,12 +77,21 @@ public class NamingServerServiceImpl {
                 if (serviceEntry.getServiceName().equals(serviceName)) {
                     for (ServerEntry serverEntry : serviceEntry.getServiceEntriesList()) {
                         if (serverEntry.getType().equals(type)) {
-                            result.add(serverEntry.getHost() + ":" + serverEntry.getPort());
+                            server = serverEntry.getHost() + ":" + serverEntry.getPort();
+                            response.addServers(server);
                         }
                     }
                 }
             }
         }
+
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
+
+        if (this.debugFlag) {
+            namingServer.debug("Ended lookup request");
+        }
+
     }
 
 }
